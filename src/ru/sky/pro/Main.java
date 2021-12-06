3010213d8d6bd32399db2ece4af742683cce48da
owlru <valujenich@inbox.ru>
2021-12-07 00:47:34 +0300

    вопрос по скобкам

--- src/ru/sky/pro/Main.java
+++ src/ru/sky/pro/Main.java
@@ -1,8 +1,32 @@
 package ru.sky.pro;
 
 public class Main {
+        public static void main(String[] args) {
+        }
+    int[] knigaRashodov = generateRandomArray();
 
-    public static void main(String[] args) {
-	// write your code here
+    public static int[] generateRandomArray() {
+
+        // создание массива случайных чисел
+        java.util.Random random = new java.util.Random();
+        int[] knigaRashodov = new int[30];
+        for (int i = 0; i < knigaRashodov.length; i++) {
+            knigaRashodov[i] = random.nextInt(100_000) + 100_000;
+        }
+        return knigaRashodov;
     }
+
+        {    //Задание 1
+    System.out.println("Задание 1");
+
+
+
+    }
+
+
 }
+
+
+
+
+
